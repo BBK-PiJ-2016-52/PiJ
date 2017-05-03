756b850e2b06ad3794a8cd1e8e0d4d784212e897
BBK-PiJ-2016-52 <UrbanTerror41>
2017-05-03 16:06:27 +0100

    Creating new method for the Linked List.

--- Day06/HospitalManager.java
+++ Day06/HospitalManager.java
@@ -1,4 +1,14 @@
 public class HospitalManager {
-private Patient firstPatient = null;
-// other methods come here...
+   private Patient firstPatient = null;
+   
+   // this is a member method of class HospitalManager
+   public void addPatient(Patient newPatient) {
+   Patient current = this.firstPatient;
+      while (current.getNextPatient() != null) {
+      // this means we are not yet at the end of the list
+      current = current.getNextPatient();
+      }
+   // at this point, current points to the last patient
+   current.setNextPatient(newPatient);
+   }
 }
