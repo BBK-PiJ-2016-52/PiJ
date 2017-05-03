e8a307d47bab037be60751f8c8ed1e1b133d5a5f
BBK-PiJ-2016-52 <UrbanTerror41>
2017-05-03 16:14:00 +0100

    Added the option to delete a patient

--- Day06/HospitalManager.java
+++ Day06/HospitalManager.java
@@ -14,5 +14,29 @@ public class HospitalManager {
       }
    // at this point, current points to the last patient
    current.setNextPatient(newPatient);
-   }
+   }
+
+   returns true if the patient was found and removed, false otherwise
+   public boolean deletePatient(String name) {
+   if (firstPatient == null) {
+   // list is empty, nothing to remove
+   return false;
+   }
+   if (firstPatient.getName().equals(name)) {
+   // first patient in the list must be removed
+   firstPatient = firstPatient.getNextPatient();
+   return true;
+   }
+   Patient current = firstPatient;
+   while (current.getNextPatient() != null) {
+   if (current.getNextPatient().getName().equals(name)) {
+   // We found it! It is the next one!
+   // Now link this patient to the one after the next
+   current.setNextPatient(current.getNextPatient().getNextPatient());
+   return true;
+   }
+   current = current.getNextPatient();
+   }
+   return false;
+   }
 }
