aca404b673d632316f260ffbd91b716dfb3024e2
BBK-PiJ-2016-52 <UrbanTerror41>
2017-05-08 15:23:51 +0100

    exercises/Day20

--- Day20/Example.java
+++ Day20/Example.java
@@ -1,47 +1,21 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class Example {
-    public static void main(String[] args) {
-// Create a list of Integer values.
-        ArrayList<Integer> myList = new ArrayList<>();
-        myList.add(7);
-        myList.add(18);
-        myList.add(10);
-        myList.add(24);
-        myList.add(17);
-        myList.add(5);
-        System.out.println("Original list: " + myList);
-// Obtain a Stream to the array list.
-        Stream<Integer> myStream = myList.stream();
-// Obtain the minimum and maximum value by uses of min(),
-// max(), isPresent(), and get().
-        Optional<Integer> minVal = myStream.min(Integer::compare);
-        if (minVal.isPresent())
-            System.out.println("Minimum value: " + minVal.get());
-// Must obtain a new stream because previous call to min()
-// is a terminal operation that consumed the stream.
-        myStream = myList.stream();
-        Optional<Integer> maxVal = myStream.max(Integer::compare);
-        if (maxVal.isPresent())
-            System.out.println("Maximum value: " + maxVal.get());
-// Sort the stream by use of sorted().
-        Stream<Integer> sortedStream = myList.stream().sorted();
-// Display the sorted stream by use of forEach().
-        System.out.print("Sorted stream: ");
-        sortedStream.forEach((n) -> System.out.print(n + " "));
-        System.out.println();
-// Display only the odd values by use of filter().
-        Stream<Integer> oddVals = myList.stream().sorted()
-                .filter((n) -> (n % 2) == 1);
-        System.out.print("Odd values: ");
-        oddVals.forEach((n) -> System.out.print(n + " "));
-        System.out.println();
-// Display only the odd values that are greater than 5. Notice that
-// two filter operations are pipelined.
-        oddVals = myList.stream().filter((n) -> (n % 2) == 1)
-                .filter((n) -> n > 5);
-        System.out.print("Odd values greater than 5: ");
-        oddVals.forEach((n) -> System.out.print(n + " "));
-        System.out.println();
-    }
+  public static void main(String[] args) {
+    List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5);
+
+    double result =
+      numbers1
+        .stream()
+        //.parallel()
+        .map(a -> a * 2.0) // 2.0, 4.0, 6.0, 8.0, 10.0
+        .reduce(1.0,(a,b) -> a * b);
+    // 1.0 * 2.0 => 2.0
+    // 2.0 * 4.0 => 8.0
+    // 8.0 * 6.0 => 48.0
+    // 48.0 * 8.0 => 384.0
+    // 384.0 * 10.0 => 3840.0
+    System.out.println(result);
+  }
 }
